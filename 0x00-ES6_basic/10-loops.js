3dee6eb6ea995cea7bb9d2f9d98fa9af99891d0b
Wamoka <elizabethwamoka@gmail.com>
2022-04-06 12:02:04 +0000

    For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const result = [];
+    for (const el of array) {
+      result.push(appendString + el);
+    }
+  
+    return result;
+  }
+  
