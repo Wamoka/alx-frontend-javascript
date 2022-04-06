c5e0da8f59ae1481fd74eebbe59e82d9444f9bb1
Wamoka <elizabethwamoka@gmail.com>
2022-04-06 12:07:03 +0000

    Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: { ...employeesList },
+      getNumberOfDepartments(depts) {
+        return Object.keys(depts).length;
+      },
+    };
+  }
+  
