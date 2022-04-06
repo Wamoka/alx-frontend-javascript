1e5a3774d2143c1c0b0c9ad3bf94021911eca2f3
Wamoka <elizabethwamoka@gmail.com>
2022-04-06 12:11:36 +0000

    Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,9 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let employeesName = '';
+    reportWithIterator.forEach((name, index) => {
+      employeesName += name;
+      if (index !== reportWithIterator.length - 1) employeesName += ' | ';
+    });
+    return employeesName;
+  }
+  
